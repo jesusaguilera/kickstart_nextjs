43eb55d1f9dcc8e923905d94f478180ea83a510f
jesusaguilera <jesusaguilera09@gmail.com>
2020-10-08 09:20:24 +0200

    useMotionValue and global.scss

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 // Dependencies
 import Link from "next/link";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 
 // Components
 import Header from "../components/Header";
@@ -7,10 +8,25 @@ import MainLayout from "../components/MainLayout";
 import Footer from "../components/Footer";
 
 export default function Home() {
+  const x = useMotionValue(0);
+  const input = [-500, 0, 200];
+  const output = [0, 1, 0];
+  const colorOutput = ["#f00", "#00f", "#0f0"];
+  const opacity = useTransform(x, input, output);
+  const color = useTransform(x, input, colorOutput);
+
   return (
     <div>
       <MainLayout>
         <h1>Welcome!</h1>
+        <motion.div
+          drag="x"
+          style={{ x, opacity, height: 30, width: 300, backgroundColor: color }}
+          onDrag={() => {
+            console.log("Get", x.get());
+            console.log("Velocity", x.getVelocity());
+          }}
+        />
         <Link href="/about">
           <a>About</a>
         </Link>
