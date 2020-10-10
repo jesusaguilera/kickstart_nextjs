951c9d65163da823787978aac6489b0cdb35050f
jesusaguilera <jesusaguilera09@gmail.com>
2020-10-10 12:27:00 +0200

    useWindowSize.height

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -1,10 +1,23 @@
 // Dependencies
+import { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import { motion } from "framer-motion";
 
+// Hooks
+import useWindowSize from "../hooks/useWindowSize";
+
 const MainLayout = (props) => {
   let wrapperClasses = classNames("o-wrapper", props.wrapperClass);
+
+  let wrapper = useRef(null);
+
+  let windowSize = useWindowSize();
+
+  useEffect(()=> {
+    document.body.style.height = `${wrapper.current.getBoundingClientRect().height}px`
+  }, [windowSize.height])
+
 
   return (
     <motion.main
@@ -12,6 +25,7 @@ const MainLayout = (props) => {
       animate="animate"
       exit={{ opacity: 0 }}
       className={wrapperClasses}
+      ref={wrapper}
     >
       {props.children}
     </motion.main>
